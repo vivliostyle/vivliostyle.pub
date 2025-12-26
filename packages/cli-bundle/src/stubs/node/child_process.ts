7897ee1b1cd5f4bf860324513a21850ec4dc2ac8
spring-raining <harusamex.com@gmail.com>
2025-12-26 10:08:00 +0900

    chore: Update @vivliostyle/cli and cli-bundle stubs

--- packages/cli-bundle/src/stubs/node/child_process.ts
+++ packages/cli-bundle/src/stubs/node/child_process.ts
@@ -3,9 +3,13 @@ const noop = () => {};
 export const exec = noop;
 export const execFile = noop;
 export const execSync = noop;
+export const spawn = noop;
+export const spawnSync = noop;
 
 export default {
   exec,
   execFile,
   execSync,
+  spawn,
+  spawnSync,
 };
